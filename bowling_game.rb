fc195d928d54922da083ef3af9cd441611383588
tinasoul <tinapshah@gmail.com>
2013-08-01 10:25:05 -0500

    Throwing a spare method working.

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,13 +1,38 @@
 class BowlingGame
+
   def initialize
     @rolls = []
   end
 
+# Record a roll in the game.
+# pins - The Integer number of pins knocked down in this roll.
+# Returns nothing
+
   def roll(pins)
+    @rolls.push(pins)
   end
 
+# Returns the Integer score for this game.
+  
   def score
-    0
-  end
+   total_score  = 0
+   current_roll = 0
 
+   while current_roll < @rolls.size - 1
+     roll      = @rolls[current_roll]
+     next_roll = @rolls[current_roll + 1]
+
+     if roll + next_roll == 10
+       total_score += 10 + @rolls[current_roll + 2]
+     else
+       total_score += roll + next_roll
+     end
+
+     current_roll += 2
+   end
+
+   return total_score
+  end
 end
+
+ 
--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -12,4 +12,20 @@ describe BowlingGame do
       expect(game.score).to eq(0)
     end
   end
+
+  context 'when one pin is knocked down on each roll' do
+    it 'scores the game as 20' do
+      20.times {game.roll(1)}
+      expect(game.score).to eq(20)
+    end
+  end 
+
+  context 'when a spare is thrown' do
+    it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+    3.times  { game.roll(5) }
+    17.times { game.roll(0) }
+
+    expect(game.score).to eq(20)
+    end
+  end
 end
